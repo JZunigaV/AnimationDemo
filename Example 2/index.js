ce73288463e46fc568cd8bb8e928e5ae265d6b2e
JzunigaV <j_zuniga4@hotmail.com>
2018-11-27 10:21:56 -0600

    Ejemplos

--- Example 2/index.js
+++ Example 2/index.js
@@ -1,58 +1,89 @@
 
+    function gameLoop() {
+
+        window.requestAnimationFrame(gameLoop);
+
+        coin.update();
+        coin.render();
+    }
 
 
-var coinImage = new Image();
-coinImage.src = "coin.png";
+    function sprite(options) {
 
-var canvas = document.getElementById("coinAnimation");
-canvas.width = 200;
-canvas.height = 200;
+        var that = {},
+            frameIndex = 0,
+            tickCount = 0,
+            ticksPerFrame = options.ticksPerFrame || 0,
+            numberOfFrames = options.numberOfFrames || 1;
 
-var coin = sprite({
-    context: canvas.getContext("2d"),
-    width: 100,
-    height: 100,
-    image: coinImage
-});
+        that.context = options.context;
+        that.width = options.width;
+        that.height = options.height;
+        that.image = options.image;
+        
 
+        that.update = function () {
 
-coinImage.addEventListener("load", loadImage, false);
+            tickCount += 1;
 
-function loadImage(e) {
-    coin.render();
-}
+            if (tickCount > ticksPerFrame) {
 
+                tickCount = 0;
 
+                // If the current frame index is in range
+                if (frameIndex < numberOfFrames - 1) {
+                    //Go to the next Frame
+                    frameIndex += 1;
+                } else {
+                    frameIndex = 0;
+                }
 
-
-
-
-
-function sprite(options) {
-
-
-    var that = {};
-    that.context = options.context;
-    that.width = options.width;
-    that.height = options.height;
-    that.image = options.image;
-    that.render = function () {
-
-        // Draw the animation
-        that.context.drawImage(
-            that.image,
-            0,
-            0,
-            that.width,
-            that.height,
-            0,
-            0,
-            that.width,
-            that.height);
-    };
+            }
+        };
 
 
-    return that;
+        that.render = function () {
 
-}
+            // Clear the canvas
+            that.context.clearRect(0, 0, that.width, that.height);
 
+            // Draw the animation
+            that.context.drawImage(
+                that.image,
+                frameIndex * that.width / numberOfFrames,
+                0,
+                that.width / numberOfFrames,
+                that.height,
+                0,
+                0,
+                that.width / numberOfFrames,
+                that.height);
+        };
+
+        return that;
+    }
+
+    //Get canvas
+    var canvas = document.getElementById("coinAnimation");
+    canvas.width = 100;
+    canvas.height = 100;
+
+
+    //Create SpriteSheet
+    var coinImage = new Image();
+
+
+    //Create Sprite
+    var coin = sprite({
+        context: canvas.getContext("2d"),
+        width: 1000,
+        height: 100,
+        image: coinImage,
+        numberOfFrames: 10,
+        ticksPerFrame: 3
+
+    });
+
+    // Load SpriteSheet
+    coinImage.addEventListener("load", gameLoop);
+    coinImage.src = "coin.png";
